bb8dea454446d3430abd05fedd036ce54b99ed50
derinsola16 <aderinsolaodusany@gmail.com>
2021-01-20 20:25:50 +0100

    Added CORS prevention code

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 // load Express.js
 const express = require("express");
 const app = express();
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // parse the request parameters
 app.use(express.json());
 // connect to MongoDB
